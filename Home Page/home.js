ca9dff05f67561d8c4b72787f31e41cf13d1c3ca
Ammarsaeed7455 <151685362+Ammarsaeed7455@users.noreply.github.com>
2024-09-27 00:01:38 +0300

    Add files via upload

---
+++ Home Page/home.js
@@ -0,0 +1,384 @@
+let products = {
+    Burgers: [
+        {
+            name:"Burger 1",
+            imgsrc:"../assets/pngtree-isolated-burger-on-transparent-background-png-image_9055072.png",
+            price: 100,
+            id:1,
+            quantanty:0,
+
+        },    
+        {
+            name:"Burger 2",
+            imgsrc:"../assets/pngtree-isolated-burger-on-transparent-background-png-image_9055072.png",
+            price: 45,
+            id:2,
+            quantanty:0,
+
+        },    
+        {
+            name:"Burger 3",
+            imgsrc:"../assets/pngtree-isolated-burger-on-transparent-background-png-image_9055072.png",
+            price: 31,
+            id:3,
+            quantanty:0,
+
+        },    
+        {
+            name:"Burger 4",
+            imgsrc:"../assets/pngtree-isolated-burger-on-transparent-background-png-image_9055072.png",
+            price: 82,
+            id:4,
+            quantanty:0,
+
+        },    
+        {
+            name:"Burger 5",
+            imgsrc:"../assets/pngtree-isolated-burger-on-transparent-background-png-image_9055072.png",
+            price: 50,
+            id:5,
+            quantanty:0,
+
+        },    
+        {
+            name:"Burger 6",
+            imgsrc:"../assets/pngtree-isolated-burger-on-transparent-background-png-image_9055072.png",
+            price: 86,
+            id:6,
+            quantanty:0,
+
+        },    
+        {
+            name:"Burger 7",
+            imgsrc:"../assets/pngtree-isolated-burger-on-transparent-background-png-image_9055072.png",
+            price: 35,
+            id:7,
+            quantanty:0,
+
+        }, 
+        {
+            name:"Burger 8",
+            imgsrc:"../assets/pngtree-isolated-burger-on-transparent-background-png-image_9055072.png",
+            price: 34,
+            id:8,
+            quantanty:0,
+
+        }    
+
+    ],
+    Drinks: [
+        {
+            name:"cocacola 1",
+            imgsrc:"../assets/Coca-Cola.png.crdownload",
+            price: 110,
+            id:1,
+            quantanty:0,
+
+        },    
+        {
+            name:"cocacola 2",
+            imgsrc:"../assets/Coca-Cola.png.crdownload",
+            price: 45,
+            id:1,
+            quantanty:0,
+
+        },    
+        {
+            name:"cocacola 3",
+            imgsrc:"../assets/Coca-Cola.png.crdownload",
+            price: 31,
+            id:1,
+            quantanty:0,
+
+        },    
+        {
+            name:"cocacola 4",
+            imgsrc:"../assets/Coca-Cola.png.crdownload",
+            price: 82,
+            id:1,
+            quantanty:0,
+
+        },    
+        {
+            name:"cocacola 5",
+            imgsrc:"../assets/Coca-Cola.png.crdownload",
+            price: 50,
+            id:1,
+            quantanty:0,
+
+        },    
+        {
+            name:"cocacola 6",
+            imgsrc:"../assets/Coca-Cola.png.crdownload",
+            price: 86,
+            id:1,
+            quantanty:0,
+
+        },    
+        {
+            name:"cocacola 7",
+            imgsrc:"../assets/Coca-Cola.png.crdownload",
+            price: 35,
+            id:1,
+            quantanty:0,
+
+        }, 
+        {
+            name:"cocacola 8",
+            imgsrc:"../assets/Coca-Cola.png.crdownload",
+            price: 34,
+            id:1,
+            quantanty:0,
+
+        }    
+
+    ],
+    Pizzas: [
+        {
+            name:"Pizza 1",
+            imgsrc:"../assets/pizza_PNG7132.png",
+            price: 100,
+            id:1,
+            quantanty:0,
+
+        },    
+        {
+            name:"Pizza 2",
+            imgsrc:"../assets/pizza_PNG7132.png",
+            price: 45,
+            id:1,
+            quantanty:0,
+
+        },    
+        {
+            name:"Pizza 3",
+            imgsrc:"../assets/pizza_PNG7132.png",
+            price: 31,
+            id:1,
+            quantanty:0,
+
+        },    
+        {
+            name:"Pizza 4",
+            imgsrc:"../assets/pizza_PNG7132.png",
+            price: 82,
+            id:1,
+            quantanty:0,
+
+        },    
+        {
+            name:"Pizza 5",
+            imgsrc:"../assets/pizza_PNG7132.png",
+            price: 50,
+            id:1,
+            quantanty:0,
+
+        },    
+        {
+            name:"Pizza 6",
+            imgsrc:"../assets/pizza_PNG7132.png",
+            price: 86,
+            id:1,
+            quantanty:0,
+
+        },    
+        {
+            name:"Pizza 7",
+            imgsrc:"../assets/pizza_PNG7132.png",
+            price: 35,
+            id:1,
+            quantanty:0,
+
+        }, 
+        {
+            name:"Pizza 8",
+            imgsrc:"../assets/pizza_PNG7132.png",
+            price: 34,
+            id:1,
+            quantanty:0,
+        }    
+
+    ]
+
+}
+let listcart = [
+        {
+            name:"Burger 1",
+            imgsrc:"../assets/pngtree-isolated-burger-on-transparent-background-png-image_9055072.png",
+            price: 100,
+            id:1,
+            quantanty:0,
+
+        },    
+        {
+            name:"Burger 2",
+            imgsrc:"../assets/pngtree-isolated-burger-on-transparent-background-png-image_9055072.png",
+            price: 45,
+            id:2,
+            quantanty:0,
+
+        },    
+        {
+            name:"Burger 3",
+            imgsrc:"../assets/pngtree-isolated-burger-on-transparent-background-png-image_9055072.png",
+            price: 31,
+            id:3,
+            quantanty:0,
+
+        },    
+        {
+            name:"Burger 4",
+            imgsrc:"../assets/pngtree-isolated-burger-on-transparent-background-png-image_9055072.png",
+            price: 82,
+            id:4,
+            quantanty:0,
+
+        },    
+        {
+            name:"Burger 5",
+            imgsrc:"../assets/pngtree-isolated-burger-on-transparent-background-png-image_9055072.png",
+            price: 50,
+            id:5,
+            quantanty:0,
+
+        },    
+        {
+            name:"Burger 6",
+            imgsrc:"../assets/pngtree-isolated-burger-on-transparent-background-png-image_9055072.png",
+            price: 86,
+            id:6,
+            quantanty:0,
+
+        },    
+        {
+            name:"Burger 7",
+            imgsrc:"../assets/pngtree-isolated-burger-on-transparent-background-png-image_9055072.png",
+            price: 35,
+            id:7,
+            quantanty:0,
+
+        }, 
+        {
+            name:"Burger 8",
+            imgsrc:"../assets/pngtree-isolated-burger-on-transparent-background-png-image_9055072.png",
+            price: 34,
+            id:8,
+            quantanty:0,
+
+        }    
+
+    ]
+    let CateProducts = document.querySelector(".cateproducts")
+    let cart = document.querySelector('.cart')
+    let cat = document.querySelector(".cat")
+    let roadsname = document.querySelector(".back")
+    let copy = Object.keys(products)
+    let showBtn = document.querySelector(".shop")
+    let closeBtn = document.querySelector('.close')
+    closeBtn.onclick = function(){
+        cart.classList.remove("show")
+    }
+    showBtn.onclick = function(){
+        cart.classList.add("show")
+
+    }
+    copy.forEach((element,index) => {
+        cat.innerHTML += `
+       <div class="div1"  onclick="movetoburger('${element}')">
+            <img src="${products[`${element}`][index].imgsrc}">
+            <h3>${products[`${element}`][index].name}</h3>
+        </div>
+    `
+    
+    })
+    
+function movetoburger(catSelect){
+    let productIdd = products[catSelect]
+    CateProducts.innerHTML = ""
+    roadsname.innerHTML = `<i class="fa-solid fa-arrow-left"></i> <span>Food & Drinks   > </span> Categories > Burgers `
+    productIdd.forEach((el,index) => {
+        CateProducts.innerHTML += `
+
+               <div class="div1">
+                <img src="${el.imgsrc}">
+                <h3>${el.name}</h3>
+                <span><h3>${el.price}</h3></span>
+                <button onclick="addtocart('${catSelect}',${index})">Get</button>
+                </div>
+            `
+
+    })
+
+
+}
+let items = document.querySelector(".items")
+let totalinner = document.querySelector("#totalprice")
+let div = document.createElement("div")
+let total = 0
+
+function getButton(){
+    items.innerHTML = ""
+    listcart.forEach((el,index) =>{
+        if(el.quantanty == 0){
+
+
+        }else{
+
+            items.innerHTML += `
+            <div class="item1"><h6>${el.name}</h6> <h6>${el.price * el.quantanty}</h6>  <div id="minus" onclick="minusFun(${index})">-</div> <div class="quantanty">${el.quantanty}</div> <div id="plus" onclick="plusFun(${el.quantanty + 1},${index})">+</div></div>`
+    
+        }
+
+    })
+}
+
+function addtocart(key,index){
+    let selectItem = products[key][index]
+    let isValid = listcart.findIndex((el,index)=>{
+        return el.name == selectItem.name
+    })
+    if (isValid == -1){
+        selectItem.quantanty = 1
+        listcart.push(selectItem)
+    }else{
+        listcart[isValid].quantanty++
+    }
+
+    total += selectItem.price
+    totalinner.innerHTML = total
+    getButton()
+
+}
+ 
+function minusFun(index){
+    listcart[index].quantanty--
+    console.log(listcart[index].name)
+    total -= listcart[index].price
+    totalinner.innerHTML = total
+    
+    getButton()
+
+    if(listcart[index].quantanty == 0){
+        items.children[index].style.display = "none"
+    
+
+    }else{
+        items.children[index].style.display = "flex"
+
+
+    }
+
+
+items.children[index].innerHTML = `
+<h6>${listcart[index].name}</h6> <h6>${listcart[index].price * listcart[index].quantanty}</h6>  <div id="minus" onclick="minusFun(${index})">-</div> <div class="quantanty">${listcart[index].quantanty}</div> <div id="plus" onclick="plusFun(${listcart[index].quantanty + 1},${index})">+</div>`
+
+}
+function plusFun(quantanty,index){
+    listcart[index].quantanty++
+    items.children[index].innerHTML = `<h6>${listcart[index].name}</h6> <h6>${listcart[index].price * listcart[index].quantanty}</h6>  <div id="minus" onclick="minusFun(${index})">-</div> <div class="quantanty">${listcart[index].quantanty}</div> <div id="plus" onclick="plusFun(${listcart[index].quantanty + 1},${index})">+</div>`
+    
+
+
+console.log(listcart[index].quantanty)
+
+}
